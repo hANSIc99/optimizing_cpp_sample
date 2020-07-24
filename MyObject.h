109517af5bef205bcc00a6cbba80cea432a24266
Stephan <stephan@localhost.localdomain>
2020-07-24 20:31:52 +0200

    added additional constructors to MyObject type

--- MyObject.h
+++ MyObject.h
@@ -8,15 +8,40 @@ template <class T>
 class MyObject{
 public:
     
+
+    /* Default constructor */
+
     MyObject(const T& mytype):
         m_mytype(mytype){
+    };
+
+
+    /* Copy constructor */
+    MyObject(const MyObject& other):
+        m_mytype(other.m_mytype){
+    };
+
+    /* Copy assignment */
+    MyObject& operator=(const MyObject& other){
+        if(this != &other){
+            m_mytype = other.m_mytype;
+        }
+        return *this;
+    };
+
+    /* Move contructor */
+    MyObject(MyObject&& other):
+        m_mytype(std::move(other.m_mytype)){
     }
 
-    /* TODO
-    MyObject(T&& mytype):
-        m_mytype(std::move(mytype)){
+    /* Move assignment */
+
+    MyObject& operator=(MyObject&& other){
+        if(this != &other){
+            m_mytype(std::move(other.m_mytype));
+        }
+        return *this;
     }
-    */
 
     T m_mytype;
 };
